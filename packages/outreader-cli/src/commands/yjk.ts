8273bdef30045a6ffd12bfcd32f12f18de95aa40
an-lee <an.lee.work@gmail.com>
2020-05-11 11:26:34 +0800

    :package: (package)

---
+++ packages/outreader-cli/src/commands/yjk.ts
@@ -0,0 +1,20 @@
+import { Command } from "@oclif/command";
+
+export default class Yjk extends Command {
+  static description = "Read from a YJK model";
+
+  static examples = [
+    `$ outreader yjk path/to/yjk/model
+`,
+  ];
+
+  static args = [
+    { name: "dir", required: true, description: "directory of the YJK model" },
+  ];
+
+  async run() {
+    const { args } = this.parse(Yjk);
+
+    this.log(`Reading from ${args.dir}`);
+  }
+}
--- packages/outreader-cli/test/commands/yjk.test.ts
+++ packages/outreader-cli/test/commands/yjk.test.ts
@@ -1,16 +1,16 @@
 import {expect, test} from '@oclif/test'
 
-describe('hello', () => {
+describe('yjk', () => {
   test
   .stdout()
-  .command(['hello'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
+  .command(['yjk'])
+  .it('runs yjk', ctx => {
+    expect(ctx.stdout).to.contain('Reading from')
   })
 
   test
   .stdout()
-  .command(['hello', '--name', 'jeff'])
+  .command(['yjk', '--name', 'jeff'])
   .it('runs hello --name jeff', ctx => {
     expect(ctx.stdout).to.contain('hello jeff')
   })
