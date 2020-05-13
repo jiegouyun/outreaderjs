6d276849e9f12f45795f4a31d7fe36c7ed7f452f
an-lee <an.lee.work@gmail.com>
2020-05-13 14:52:56 +0800

    :recycle: (core) file processor support multi encoding

--- packages/outreader-core/src/file-processor/file-processor.ts
+++ packages/outreader-core/src/file-processor/file-processor.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 import * as readline from "readline";
 import * as iconv from "iconv-lite";
 
@@ -8,21 +9,44 @@ import * as iconv from "iconv-lite";
  * @param processFunc: function;
  */
 export async function readLineByLine(
-  path: string,
-  processFunc: (line: string) => any
+  pathName: string,
+  processFunc: (line: string) => any,
+  options: IOptions = {}
 ) {
-  if (!fs.existsSync(path)) {
-    throw new Error(`Can not find ${path}!`);
+  if (!fs.existsSync(pathName)) {
+    throw new Error(`Can not find ${pathName}!`);
   }
 
   const rl = readline.createInterface({
-    input: fs
-      .createReadStream(path)
-      .pipe(iconv.decodeStream("gb2312"))
-      .pipe(iconv.encodeStream("utf8")),
+    input: ensureUtf8Stream(pathName, options.encoding),
   });
 
   for await (const line of rl) {
     processFunc(line);
   }
+}
+
+function ensureUtf8Stream(pathName: string, encoding?: string) {
+  const extname = path.extname(pathName);
+  let stream;
+
+  if (encoding) {
+    stream = fs
+      .createReadStream(pathName)
+      .pipe(iconv.decodeStream(encoding))
+      .pipe(iconv.encodeStream("utf8"));
+  } else if (extname === ".out" || extname === ".OUT") {
+    stream = fs
+      .createReadStream(pathName)
+      .pipe(iconv.decodeStream("gb2312"))
+      .pipe(iconv.encodeStream("utf8"));
+  } else {
+    stream = fs.createReadStream(pathName);
+  }
+
+  return stream;
+}
+
+export interface IOptions {
+  encoding?: string;
 }
