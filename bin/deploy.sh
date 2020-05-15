b6c250663c00da32658a958b201a91b38e7e2a29
an-lee <an.lee.work@gmail.com>
2020-05-15 18:34:38 +0800

    :construction_worker: (workflow) add deploy-docs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,11 +12,24 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 echo 'outreader.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jiegouyun/outreaderjs.git master:gh-pages
+if [[ ${GIHUB_TOKEN} ]]; then
+  # for Github actions
+  remote_repo="https://${GITHUB_ACTOR}:${GIHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+  git init && \
+  git config user.name "${GITHUB_ACTOR}" && \
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+  git add . && \
+  git commit -m "vuepress build from Action ${GITHUB_SHA}" && \
+  git show-ref && \
+  git push --force $remote_repo master:gh-pages && \
+  rm -fr .git && \
+else
+  # for local deploy
+  git init
+  git add -A
+  git commit -m 'vuepress build from local'
+  git push -f git@github.com:jiegouyun/outreaderjs.git master:gh-pages
+fi
 
 cd -
