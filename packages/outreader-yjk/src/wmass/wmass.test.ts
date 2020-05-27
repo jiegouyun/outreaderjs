b2f3ffc67b89c20060668fb4bef2963f278b2544
an-lee <an.lee.work@gmail.com>
2020-05-27 15:40:17 +0800

    :white_check_mark: (yjk) add information test case

--- packages/outreader-yjk/src/wmass/wmass.test.ts
+++ packages/outreader-yjk/src/wmass/wmass.test.ts
@@ -1,14 +1,21 @@
-import { readWmassOutput } from "./wmass";
+import { readWmassOutput, IWmass } from "./wmass";
 import * as path from "path";
 
 describe("wmass", () => {
   const dir = path.join(__dirname, "../../../../fixtures/yjk/1");
+  let wmass: IWmass;
+
+  beforeEach(async () => {
+    wmass = await readWmassOutput(dir);
+  });
 
   it("should read wmass.out", async () => {
-    try {
-      await readWmassOutput(dir)
-    } catch (error) {
-      console.error(error);
-    }
+    expect(wmass).toBeTruthy();
+  });
+
+  it("should extract information", async () => {
+    const information = wmass.information;
+    expect(information.engineering).toBe("071A");
+    expect(information.software).toBe("盈建科建筑结构设计软件");
   });
 });
