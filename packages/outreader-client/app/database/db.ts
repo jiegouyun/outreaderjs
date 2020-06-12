63fa45d38f1a612842990964c051e1f4950456b2
an-lee <an.lee.work@gmail.com>
2020-06-12 16:11:02 +0800

    refactor(client): rename db.json

--- packages/outreader-client/app/database/db.ts
+++ packages/outreader-client/app/database/db.ts
@@ -3,11 +3,13 @@ import path from 'path';
 import lowdb from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 
-export async function initDb() {
-  const filename = path.join(
-    (electron.app || electron.remote.app).getPath('userData'),
-    'db.json'
+export async function initDb(db: string = 'outreader') {
+  return lowdb(
+    new FileSync(
+      path.join(
+        (electron.app || electron.remote.app).getPath('userData'),
+        db + '.json'
+      )
+    )
   );
-  console.log(filename);
-  return lowdb(new FileSync(filename));
 }
