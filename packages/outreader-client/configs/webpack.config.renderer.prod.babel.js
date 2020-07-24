c9690f3af5bd3ff2c0dd2d3cba8b7e0db7d85072
an-lee <an.lee.work@gmail.com>
2020-07-24 22:24:05 +0800

    fix(client): fix build error because of webpack-merge upgrade

--- packages/outreader-client/configs/webpack.config.renderer.prod.babel.js
+++ packages/outreader-client/configs/webpack.config.renderer.prod.babel.js
@@ -7,7 +7,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import merge from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import TerserPlugin from 'terser-webpack-plugin';
 import baseConfig from './webpack.config.base';
 import CheckNodeEnv from '../internals/scripts/CheckNodeEnv';
@@ -16,7 +16,7 @@ import DeleteSourceMaps from '../internals/scripts/DeleteSourceMaps';
 CheckNodeEnv('production');
 DeleteSourceMaps();
 
-export default merge.smart(baseConfig, {
+export default merge(baseConfig, {
   devtool: process.env.DEBUG_PROD === 'true' ? 'source-map' : 'none',
 
   mode: 'production',
