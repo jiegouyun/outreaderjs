d88a6122b83df3ecdade9db8f639d9fb3178a65d
ym6 <yangml.work@gmail.com>
2020-08-04 09:58:21 +0800

    convert parameters data.

--- packages/outreader-yjk/src/structure-front-end/structure-front-end.ts
+++ packages/outreader-yjk/src/structure-front-end/structure-front-end.ts
@@ -14,6 +14,7 @@ import {
 } from '@outreader/core';
 import { convertSummary } from './summary';
 import { convertSummaryQuantity } from './summary-quantity';
+import { convertParameters } from './parameters';
 
 export async function convertStructure(
   structure: IStructure,
@@ -32,12 +33,6 @@ export async function convertStructure(
   };
 
   return structureFE;
-}
-
-function convertParameters(structure: IStructure): Promise<IParametersFE> {
-  const parameters: IParametersFE = {};
-
-  return parameters;
 }
 
 function convertPeriod(structure: IStructure): Promise<IPeriodFE> {
