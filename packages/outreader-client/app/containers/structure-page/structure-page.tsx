e109f1f77d4150e37ff7b4f96aeedb8bb5e1a6db
ym6 <yangml.work@gmail.com>
2020-08-07 16:08:35 +0800

    export parameters page.

--- packages/outreader-client/app/containers/structure-page/structure-page.tsx
+++ packages/outreader-client/app/containers/structure-page/structure-page.tsx
@@ -25,6 +25,7 @@ export function StructurePage() {
   const structureFE = convertStructure(structure);
   const summaryData = structureFE.summary;
   const summaryQuantityData = structureFE.summaryQuantity;
+  const parameters = structureFE.parameters;
   console.log(structure);
   if (!structure.hash) {
     message.error('找不到模型');
@@ -597,12 +598,145 @@ export function StructurePage() {
     </div>
   );
 
+  const Parameters = (
+    <div>
+      <Descriptions
+        title="结构总体信息"
+        bordered
+        size="small"
+        column={{ xs: 1, sm: 1 }}
+        style={{ marginBottom: 20 }}
+      >
+        <Descriptions.Item label="结构体系">
+          {parameters.general.system}
+        </Descriptions.Item>
+        <Descriptions.Item label="结构材料信息">
+          {parameters.general.material}
+        </Descriptions.Item>
+        <Descriptions.Item label="结构所在地区">
+          {parameters.general.location}
+        </Descriptions.Item>
+        <Descriptions.Item label="地下室层数">
+          {parameters.general.basement}
+        </Descriptions.Item>
+        <Descriptions.Item label="嵌固端所在层号">
+          {parameters.general.constraintFloor}
+        </Descriptions.Item>
+        <Descriptions.Item label="裙房层数">
+          {parameters.general.podium}
+        </Descriptions.Item>
+        <Descriptions.Item label="转换层所在层号">
+          {parameters.general.transferStorey}
+        </Descriptions.Item>
+        <Descriptions.Item label="加强层所在层号">
+          {parameters.general.reinforceStorey}
+        </Descriptions.Item>
+      </Descriptions>
+      <Descriptions
+        title="计算控制信息"
+        bordered
+        size="small"
+        column={{ xs: 1, sm: 1 }}
+        style={{ marginBottom: 20 }}
+      >
+        <Descriptions.Item label="连梁刚度折减系数(地震)">
+          {parameters.calculate.couplingBeamFactorSeismic}
+        </Descriptions.Item>
+        <Descriptions.Item label="连梁刚度折减系数(风)">
+          {parameters.calculate.couplingBeamFactorWind}
+        </Descriptions.Item>
+        <Descriptions.Item label="刚性楼板假定">
+          {parameters.calculate.rigidFloorAssumption}
+        </Descriptions.Item>
+      </Descriptions>
+      <Descriptions
+        title="风荷载信息"
+        bordered
+        size="small"
+        column={{ xs: 1, sm: 1 }}
+        style={{ marginBottom: 20 }}
+      >
+        <Descriptions.Item label="使用指定风荷载数据">
+          {parameters.wind.assigned}
+        </Descriptions.Item>
+        <Descriptions.Item label="执行规范">
+          {parameters.wind.loadCode}
+        </Descriptions.Item>
+        <Descriptions.Item label="地面粗糙程度">
+          {parameters.wind.terrainRoughness}
+        </Descriptions.Item>
+        <Descriptions.Item label="修正后基本风压(kN/m^2)">
+          {parameters.wind.pressureModified}
+        </Descriptions.Item>
+        <Descriptions.Item label="风荷载计算阻尼比">
+          {parameters.wind.dampingRatio}
+        </Descriptions.Item>
+        <Descriptions.Item label="舒适度验算基本风压(kN/m^2)">
+          {parameters.wind.pressureComfort}
+        </Descriptions.Item>
+        <Descriptions.Item label="舒适度验算阻尼比">
+          {parameters.wind.dampingRationComfort}
+        </Descriptions.Item>
+      </Descriptions>
+      <Descriptions
+        title="地震信息"
+        bordered
+        size="small"
+        column={{ xs: 1, sm: 1 }}
+        style={{ marginBottom: 20 }}
+      >
+        <Descriptions.Item label="按地震动区划图GB18306-2015计算">
+          {parameters.seismic.use2015GB18306}
+        </Descriptions.Item>
+        <Descriptions.Item label="设计地震分组">
+          {parameters.seismic.group}
+        </Descriptions.Item>
+        <Descriptions.Item label="地震烈度">
+          {parameters.seismic.intensity}
+        </Descriptions.Item>
+        <Descriptions.Item label="场地类别">
+          {parameters.seismic.siteCategory}
+        </Descriptions.Item>
+        <Descriptions.Item label="特征周期">
+          {parameters.seismic.characteristicPeriod}
+        </Descriptions.Item>
+        <Descriptions.Item label="阻尼比">
+          {parameters.seismic.dampingRatio}
+        </Descriptions.Item>
+        <Descriptions.Item label="周期折减系数">
+          {parameters.seismic.periodReductionFactor}
+        </Descriptions.Item>
+        <Descriptions.Item label="X向偶然偏心">
+          {parameters.seismic.eccentricityX}
+        </Descriptions.Item>
+        <Descriptions.Item label="Y向偶然偏心">
+          {parameters.seismic.eccentricityY}
+        </Descriptions.Item>
+        <Descriptions.Item label="地震影响系数最大值">
+          {parameters.seismic.maxSpectrumValue}
+        </Descriptions.Item>
+        <Descriptions.Item label="罕遇地震影响系数最大值">
+          {parameters.seismic.maxSpectrumValueL3}
+        </Descriptions.Item>
+        <Descriptions.Item label="最大附加阻尼比">
+          {parameters.seismic.additionalDampingRatio}
+        </Descriptions.Item>
+        <Descriptions.Item label="调整后的水平向减震系数">
+          {parameters.seismic.modifiedSeismicReductionFactor}
+        </Descriptions.Item>
+      </Descriptions>
+    </div>
+  );
+
   const dataMapping = {
     summary: () => {
       return Summary;
     },
     summaryQuantity: () => {
       return SummaryQuantity;
+    },
+    parameters: () => {
+      return Parameters;
     },
   };
 
