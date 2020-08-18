bb93dd5bc456aecf811a781bf906663330cf5002
ym6 <yangml.work@gmail.com>
2020-08-18 10:14:44 +0800

    update.

--- packages/outreader-yjk/src/structure-front-end/parameters.ts
+++ packages/outreader-yjk/src/structure-front-end/parameters.ts
@@ -6,20 +6,17 @@ export function convertParameters(structure: IStructure): IParametersFE {
       system: structure.wmass?.generalInformation.structuralSystem || '',
       material: structure.wmass?.generalInformation.structuralMaterial || '',
       location: structure.wmass?.generalInformation.location || '',
-      basement: structure.wmass?.generalInformation.basement as number,
-      constraintFloor: structure.wmass?.generalInformation
-        .constraintFloor as number,
-      podium: structure.wmass?.generalInformation.podium as number,
-      transferStorey: structure.wmass?.generalInformation
-        .transferStorey as number,
-      reinforceStorey: structure.wmass?.generalInformation
-        .reinforceStorey as number,
+      basement: structure.wmass?.generalInformation.basement || 0,
+      constraintFloor: structure.wmass?.generalInformation.constraintFloor || 0,
+      podium: structure.wmass?.generalInformation.podium || 0,
+      transferStorey: structure.wmass?.generalInformation.transferStorey || 0,
+      reinforceStorey: structure.wmass?.generalInformation.reinforceStorey || 0,
     },
     calculate: {
-      couplingBeamFactorSeismic: structure.wmass?.calculationControl
-        .couplingBeamFactorSeismic as number,
-      couplingBeamFactorWind: structure.wmass?.calculationControl
-        .couplingBeamFactorWind as number,
+      couplingBeamFactorSeismic:
+        structure.wmass?.calculationControl.couplingBeamFactorSeismic || 0,
+      couplingBeamFactorWind:
+        structure.wmass?.calculationControl.couplingBeamFactorWind || 0,
       rigidFloorAssumption:
         structure.wmass?.calculationControl.rigidFloorAssumption || '',
     },
@@ -27,36 +24,32 @@ export function convertParameters(structure: IStructure): IParametersFE {
       assigned: structure.wmass?.windInformation.useAssigned || '',
       loadCode: structure.wmass?.windInformation.loadCode || '',
       terrainRoughness: structure.wmass?.windInformation.terrainRoughness || '',
-      pressureModified: structure.wmass?.windInformation
-        .pressureModified as number,
-      dampingRatio: structure.wmass?.windInformation.dampingRatio as number,
-      pressureComfort: structure.wmass?.windInformation
-        .pressureComfort as number,
-      dampingRationComfort: structure.wmass?.windInformation
-        .dampingRationComfort as number,
+      pressureModified: structure.wmass?.windInformation.pressureModified || 0,
+      dampingRatio: structure.wmass?.windInformation.dampingRatio || 0,
+      pressureComfort: structure.wmass?.windInformation.pressureComfort || 0,
+      dampingRationComfort:
+        structure.wmass?.windInformation.dampingRationComfort || 0,
     },
     seismic: {
       use2015GB18306: structure.wmass?.seismicInformation.use2015GB18306 || '',
       group: structure.wmass?.seismicInformation.group || '',
       intensity: structure.wmass?.seismicInformation.intensity || '',
       siteCategory: structure.wmass?.seismicInformation.siteCategory || '',
-      characteristicPeriod: structure.wmass?.seismicInformation
-        .characteristicPeriod as number,
-      dampingRatio: structure.wmass?.seismicInformation.dampingRatio as number,
-      periodReductionFactor: structure.wmass?.seismicInformation
-        .periodReductionFactor as number,
-      eccentricityX: structure.wmass?.seismicInformation
-        .eccentricityX as number,
-      eccentricityY: structure.wmass?.seismicInformation
-        .eccentricityY as number,
-      maxSpectrumValue: structure.wmass?.seismicInformation
-        .maxSpectrumValue as number,
-      maxSpectrumValueL3: structure.wmass?.seismicInformation
-        .maxSpectrumValueL3 as number,
-      additionalDampingRatio: structure.wmass?.seismicInformation
-        .additionalDampingRatio as number,
-      modifiedSeismicReductionFactor: structure.wmass?.seismicInformation
-        .modifiedSeismicReductionFactor as number,
+      characteristicPeriod:
+        structure.wmass?.seismicInformation.characteristicPeriod || 0,
+      dampingRatio: structure.wmass?.seismicInformation.dampingRatio || 0,
+      periodReductionFactor:
+        structure.wmass?.seismicInformation.periodReductionFactor || 0,
+      eccentricityX: structure.wmass?.seismicInformation.eccentricityX || 0,
+      eccentricityY: structure.wmass?.seismicInformation.eccentricityY || 0,
+      maxSpectrumValue:
+        structure.wmass?.seismicInformation.maxSpectrumValue || 0,
+      maxSpectrumValueL3:
+        structure.wmass?.seismicInformation.maxSpectrumValueL3 || 0,
+      additionalDampingRatio:
+        structure.wmass?.seismicInformation.additionalDampingRatio || 0,
+      modifiedSeismicReductionFactor:
+        structure.wmass?.seismicInformation.modifiedSeismicReductionFactor || 0,
     },
   };
 
