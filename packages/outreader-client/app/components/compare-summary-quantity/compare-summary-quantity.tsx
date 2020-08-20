4b02bd65a874ec8420967bcf05c82765a26a1b75
ym6 <yangml.work@gmail.com>
2020-08-20 16:55:31 +0800

    display compare summary quantity page.

--- packages/outreader-client/app/components/compare-summary-quantity/compare-summary-quantity.tsx
+++ packages/outreader-client/app/components/compare-summary-quantity/compare-summary-quantity.tsx
@@ -3,121 +3,93 @@ import React from 'react';
 import { ISummaryQuantityFE } from '@outreader/core';
 import { ICompare } from '../../interfaces';
 
-export function SummaryQuantityComponent(summaryQuantity: ISummaryQuantityFE) {
-  const summaryQuantityColumns = [
+export function CompareSummaryQuantityComponent(
+  summaryQuantities: ISummaryQuantityFE[]
+) {
+  const n: number = summaryQuantities.length;
+
+  const modelColumns = [
     {
       title: '分项',
-      dataIndex: 'part',
-    },
-    {
-      title: '墙',
-      dataIndex: 'wall',
-    },
-    {
-      title: '柱',
-      dataIndex: 'column',
-    },
-    {
-      title: '梁',
-      dataIndex: 'beam',
-    },
-    {
-      title: '板',
-      dataIndex: 'floor',
-    },
-    {
-      title: '合计',
-      dataIndex: 'total',
+      dataIndex: 'iterms',
     },
   ];
 
-  const summaryQuantityTableData = [
+  for (let i = 0; i < n; i++) {
+    modelColumns.push({
+      title: `模型${i + 1}`,
+      dataIndex: `model${i}`,
+    });
+  }
+
+  const quantityTableData: ICompare[] = [
     {
       key: 0,
-      part: '钢筋含量(kg/m^2)',
-      wall: summaryQuantity.unitRebar.wall.toFixed(2),
-      column: summaryQuantity.unitRebar.column.toFixed(2),
-      beam: summaryQuantity.unitRebar.beam.toFixed(2),
-      floor: summaryQuantity.unitRebar.floor.toFixed(2),
-      total: summaryQuantity.unitRebar.total.toFixed(2),
+      iterms: '结构高度(m)',
     },
     {
       key: 1,
-      part: '砼含量(m^3/m^2)',
-      wall: summaryQuantity.unitConcrete.wall.toFixed(2),
-      column: summaryQuantity.unitConcrete.column.toFixed(2),
-      beam: summaryQuantity.unitConcrete.beam.toFixed(2),
-      floor: summaryQuantity.unitConcrete.floor.toFixed(2),
-      total: summaryQuantity.unitConcrete.total.toFixed(2),
+      iterms: '结构面积(m^2)',
     },
     {
       key: 2,
-      part: '型钢含量(kg/m^2)',
-      wall: (summaryQuantity.unitSteel.wall * 1000).toFixed(2),
-      column: (summaryQuantity.unitSteel.column * 1000).toFixed(2),
-      beam: (summaryQuantity.unitSteel.beam * 1000).toFixed(2),
-      floor: (summaryQuantity.unitSteel.floor * 1000).toFixed(2),
-      total: (summaryQuantity.unitSteel.total * 1000).toFixed(2),
+      iterms: '砼总量(m^3)',
     },
     {
       key: 3,
-      part: '钢筋总量(t)',
-      wall: (summaryQuantity.rebar.wall / 1000).toFixed(0),
-      column: (summaryQuantity.rebar.column / 1000).toFixed(0),
-      beam: (summaryQuantity.rebar.beam / 1000).toFixed(0),
-      floor: (summaryQuantity.rebar.floor / 1000).toFixed(0),
-      total: (summaryQuantity.rebar.total / 1000).toFixed(0),
+      iterms: '砼含量(m^3/m^2)',
     },
     {
       key: 4,
-      part: '砼总量(m^3)',
-      wall: summaryQuantity.concrete.wall.toFixed(0),
-      column: summaryQuantity.concrete.column.toFixed(0),
-      beam: summaryQuantity.concrete.beam.toFixed(0),
-      floor: summaryQuantity.concrete.floor.toFixed(0),
-      total: summaryQuantity.concrete.total.toFixed(0),
+      iterms: '钢筋总量(t)',
     },
     {
       key: 5,
-      part: '型钢总量(t)',
-      wall: summaryQuantity.steel.wall.toFixed(0),
-      column: summaryQuantity.steel.column.toFixed(0),
-      beam: summaryQuantity.steel.beam.toFixed(0),
-      floor: summaryQuantity.steel.floor.toFixed(0),
-      total: summaryQuantity.steel.total.toFixed(0),
+      iterms: '钢筋含量(kg/m^2)',
+    },
+    {
+      key: 6,
+      iterms: '型钢总量(t)',
+    },
+    {
+      key: 7,
+      iterms: '型钢含量(kg/m^2)',
     },
   ];
 
-  const SummaryQuantity = (
+  for (let i = 0; i < n; i++) {
+    quantityTableData[0][`model${i}`] = summaryQuantities[
+      i
+    ].structure.height.toFixed(1);
+    quantityTableData[1][`model${i}`] = summaryQuantities[
+      i
+    ].structure.area.toFixed(0);
+    quantityTableData[2][`model${i}`] = summaryQuantities[
+      i
+    ].concrete.total.toFixed(0);
+    quantityTableData[3][`model${i}`] = summaryQuantities[
+      i
+    ].unitConcrete.total.toFixed(2);
+    quantityTableData[4][`model${i}`] = (
+      summaryQuantities[i].rebar.total / 1000
+    ).toFixed(0);
+    quantityTableData[5][`model${i}`] = summaryQuantities[
+      i
+    ].unitRebar.total.toFixed(2);
+    quantityTableData[6][`model${i}`] = summaryQuantities[
+      i
+    ].steel.total.toFixed(0);
+    quantityTableData[7][`model${i}`] = (
+      summaryQuantities[i].unitSteel.total * 1000
+    ).toFixed(2);
+  }
+
+  const SummaryQuantities = (
     <div>
       <h3>含钢量汇总</h3>
-      <Descriptions
-        bordered
-        size="small"
-        column={{ xs: 1, sm: 1 }}
-        style={{ marginBottom: 20 }}
-      >
-        <Descriptions.Item label="工程名称">
-          {summaryQuantity.structure.engineering}
-        </Descriptions.Item>
-        <Descriptions.Item label="结构高度(m)">
-          {summaryQuantity.structure.height.toFixed(1)}
-        </Descriptions.Item>
-        <Descriptions.Item label="结构面积(m^2)">
-          {summaryQuantity.structure.area.toFixed(0)}
-        </Descriptions.Item>
-        <Descriptions.Item label="结构周期(T1/T2/T3)">
-          {summaryQuantity.structure.period}
-        </Descriptions.Item>
-        <Descriptions.Item label="层间位移角(风/地震)">
-          {summaryQuantity.structure.drift}
-        </Descriptions.Item>
-        <Descriptions.Item label="底层墙厚(X/Y mm)">{}</Descriptions.Item>
-        <Descriptions.Item label="底层柱截面(mm)">{}</Descriptions.Item>
-      </Descriptions>
       <Table
-        columns={summaryQuantityColumns}
-        dataSource={summaryQuantityTableData}
+        columns={modelColumns}
+        dataSource={quantityTableData}
         bordered
         size="small"
         pagination={false}
@@ -139,5 +111,5 @@ export function SummaryQuantityComponent(summaryQuantity: ISummaryQuantityFE) {
     </div>
   );
 
-  return SummaryQuantity;
+  return SummaryQuantities;
 }
