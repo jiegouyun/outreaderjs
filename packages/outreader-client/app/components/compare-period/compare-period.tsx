6f0e9018721a4778b01e52b3ae3f0882c922e245
young <yangml.work@gmail.com>
2020-08-24 06:56:11 +0800

    Fix compare (#68)
    * fix bug.
    
    * fix insufficient files compare bug.

--- packages/outreader-client/app/components/compare-period/compare-period.tsx
+++ packages/outreader-client/app/components/compare-period/compare-period.tsx
@@ -40,10 +40,12 @@ export function ComparePeriodComponent(periods: IPeriodFE[]) {
       modeID: modeID[j],
     });
     for (let i = 0; i < n; i++) {
-      periodModeTableData[j][`period${i}`] =
-        periods[i].modeCoupling.period[j].toFixed(3) || 0;
-      periodModeTableData[j][`angle${i}`] =
-        periods[i].modeCoupling.angle[j].toFixed(0) || 0;
+      periodModeTableData[j][`period${i}`] = (
+        periods[i].modeCoupling.period[j] || 0
+      ).toFixed(3);
+      periodModeTableData[j][`angle${i}`] = (
+        periods[i].modeCoupling.angle[j] || 0
+      ).toFixed(0);
     }
   }
 
@@ -54,10 +56,12 @@ export function ComparePeriodComponent(periods: IPeriodFE[]) {
       modeID: j,
     });
     for (let i = 0; i < n; i++) {
-      periodSeismicTableData[j][`period${i}`] =
-        periods[i].modeSeismic.period[j].toFixed(3) || 0;
-      periodSeismicTableData[j][`angle${i}`] =
-        periods[i].modeSeismic.angle[j].toFixed(0) || 0;
+      periodSeismicTableData[j][`period${i}`] = (
+        periods[i].modeSeismic.period[j] || 0
+      ).toFixed(3);
+      periodSeismicTableData[j][`angle${i}`] = (
+        periods[i].modeSeismic.angle[j] || 0
+      ).toFixed(0);
     }
   }
 
@@ -92,12 +96,15 @@ export function ComparePeriodComponent(periods: IPeriodFE[]) {
       modeID: j,
     });
     for (let i = 0; i < n; i++) {
-      periodMassTableData[j][`factorX${i}`] =
-        periods[i].modeMass.factorX[j].toFixed(2) || 0;
-      periodMassTableData[j][`factorY${i}`] =
-        periods[i].modeMass.factorY[j].toFixed(2) || 0;
-      periodMassTableData[j][`factorZ${i}`] =
-        periods[i].modeMass.factorZ[j].toFixed(2) || 0;
+      periodMassTableData[j][`factorX${i}`] = (
+        periods[i].modeMass.factorX[j] || 0
+      ).toFixed(2);
+      periodMassTableData[j][`factorY${i}`] = (
+        periods[i].modeMass.factorY[j] || 0
+      ).toFixed(2);
+      periodMassTableData[j][`factorZ${i}`] = (
+        periods[i].modeMass.factorZ[j] || 0
+      ).toFixed(2);
     }
   }
 
