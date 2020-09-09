da983407cd0a47dc918e970bfc9328675a9d191c
ym6 <yangml.work@gmail.com>
2020-09-09 09:10:08 +0800

    save png use scroll lenth.

--- packages/outreader-core/src/download-image/download-image.ts
+++ packages/outreader-core/src/download-image/download-image.ts
@@ -1,25 +1,25 @@
 import { saveAs } from 'file-saver';
 import domtoimage from 'dom-to-image';
 export function downloadSVG(elementID: string) {
-  const chartSVG = document.getElementById(elementID) || new HTMLElement();
+  const chartSVG = document.getElementById(elementID)?.firstElementChild || new HTMLElement();
   const svgXML = new XMLSerializer().serializeToString(chartSVG);
   const svgBlob = new Blob([svgXML], { type: 'image/svg+xml;charset=utf-8' });
   saveAs(svgBlob, `${elementID}.svg`);
 }
 
 export function downloadPNG(elementID: string) {
-  const chartSVG = document.getElementById(elementID) || new HTMLElement();
+  const chartSVG = document.getElementById(elementID)?.firstElementChild || new HTMLElement();
   const scale = 5;
   const style = {
     transform: `scale(${scale})`,
     transformOrigin: 'top left',
-    width: chartSVG.offsetWidth * scale + 'px',
-    height: chartSVG.offsetHeight * scale + 'px',
+    width: chartSVG.scrollWidth * scale + 'px',
+    height: chartSVG.scrollHeight * scale + 'px',
   };
   const param = {
     bgcolor: '#ffffff',
-    height: chartSVG.offsetHeight * scale,
-    width: chartSVG.offsetWidth * scale,
+    height: chartSVG.scrollHeight * scale,
+    width: chartSVG.scrollWidth * scale,
     style,
   };
   domtoimage.toBlob(chartSVG, param).then((blob) => {
