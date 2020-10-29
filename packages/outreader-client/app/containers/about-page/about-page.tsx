db6b8f3a03367d783b4de76591af446aaadfa72b
young <yangml.work@gmail.com>
2020-10-29 13:18:57 +0800

    Fix about (#180)
    * display pkpm data in client.
    
    * export pkpm data into excel.
    
    * add software selection.
    
    * fix(client): add about page.

--- packages/outreader-client/app/containers/about-page/about-page.tsx
+++ packages/outreader-client/app/containers/about-page/about-page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Row, Divider } from 'antd';
 import { IStyles } from '../../interfaces';
+import { exec } from 'child_process';
 
 const styles: IStyles = {
   container: {
@@ -7,6 +9,49 @@ const styles: IStyles = {
     padding: '2rem',
   },
 };
+
 export function AboutPage() {
-  return <div style={styles.container}>作者信息</div>;
+  let cmd = '';
+  switch (process.platform) {
+    case 'win32':
+      cmd = 'start';
+      break;
+    case 'linux':
+      cmd = 'xdg-open';
+      break;
+    case 'darwin':
+      cmd = 'open';
+  }
+  return (
+    <div style={styles.container}>
+      <Row justify="space-around">
+        <a
+          style={{ fontSize: '4rem', color: 'darkslategray' }}
+          onClick={() => exec(`${cmd} https://outreader.com/`)}
+        >
+          <br />
+          OutReader
+        </a>
+      </Row>
+      <Row justify="space-around">
+        <p style={{ fontSize: '2rem', color: 'darkslategray' }}>
+          工程师数据一站式解决方案
+        </p>
+      </Row>
+      <Row justify="space-around">
+        <a
+          style={{ color: 'darkslategray' }}
+          onClick={() => exec(`${cmd} https://outreader.com/download/`)}
+        >
+          版本 v0.0.5
+        </a>
+      </Row>
+      <Divider />
+      <Row justify="space-around">
+        <a onClick={() => exec(`${cmd} https://outreader.com/`)}>
+          www.outreader.com
+        </a>
+      </Row>
+    </div>
+  );
 }
