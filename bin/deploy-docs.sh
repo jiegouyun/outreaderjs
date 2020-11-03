018e43ac65c5d839ac49085f5de75c4e7886ff30
an-lee <an.lee.work@gmail.com>
2020-11-03 16:09:56 +0800

    chore(deploy): deploy docs to vercel

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -10,25 +10,25 @@ yarn run build:docs
 cd docs/.vuepress/dist
 
 # add customized domain
-echo 'outreader.com' > CNAME
+# echo 'outreader.com' > CNAME
 
 
 if [[ ${GIHUB_TOKEN} ]]; then
   # for Github actions
-  remote_repo="https://${GITHUB_ACTOR}:${GIHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+  remote_repo="https://${GITHUB_ACTOR}:${GIHUB_TOKEN}@github.com/an-lee/outreader.com.git" && \
   git init && \
   git config user.name "${GITHUB_ACTOR}" && \
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
   git add . && \
   git commit -m "vuepress build from Action ${GITHUB_SHA}" && \
   git show-ref && \
-  git push --force $remote_repo master:gh-pages
+  git push --force $remote_repo master:master
 else
   # for local deploy
   git init
   git add -A
   git commit -m 'vuepress build from local'
-  git push -f git@github.com:jiegouyun/outreaderjs.git master:gh-pages
+  git push -f git@github.com:an-lee/outreader.com.git master:master
 fi
 
 rm -fr .git
